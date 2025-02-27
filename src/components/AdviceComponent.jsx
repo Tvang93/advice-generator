859e879d60b6f12280c0a1707b9865ea6b65b419
Tvang93 <vangt39@gmail.com>
2025-02-27 08:24:42 -0800

    dice relative

--- src/components/AdviceComponent.jsx
+++ src/components/AdviceComponent.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import dice from '../assets/images/icon-dice.svg'
-import dividerDesktop from '../assets/images/pattern-divider-desktop.svg'
-import dividerMobile from '../assets/images/pattern-divider-mobile.svg'
-import '../index.css'
+import React from "react";
+import dice from "../assets/images/icon-dice.svg";
+import dividerDesktop from "../assets/images/pattern-divider-desktop.svg";
+import dividerMobile from "../assets/images/pattern-divider-mobile.svg";
+import "../index.css";
 
 function AdviceComponent() {
-
-
   return (
-    <div className='relative bg-dark-grayish-blue'>
-      <div style={{fontFamily: 'Manrope'}}>
-        <h1 className='text-neon-green'>Advice #</h1>
-        <h2 className='text-light-cyan text-[28px]'>"Quote"</h2>
+    <div className="bg-dark-grayish-blue flex flex-col items-center pt-10 px-10 rounded-xl">
+      <div
+        style={{ fontFamily: "Manrope" }}
+        className="relative flex flex-col items-center gap-10 w-[444px]"
+      >
+        <h1 className="text-neon-green">Advice #</h1>
+        <h2 className="text-light-cyan text-[28px] inline-flex text-center">"Quote"</h2>
         <img src={dividerDesktop} alt="dice icon" />
       </div>
-      <div className='absolute bg-neon-green rounded-full p-6'>
-        <img src={dice} alt="" />
-      </div>
+        <div className="relative bg-neon-green rounded-full p-6 top-9">
+          <img src={dice} alt="dice icon" />
+        </div>
     </div>
-  )
+  );
 }
 
-export default AdviceComponent
+export default AdviceComponent;
